61022a4df8261bae21760ad2ce50927b82d4cc32
Eliezer Marques da Silva Neto <eliezer.neto@sec.ba.gov.br>
2019-12-19 11:46:51 -0300

    Put graphql in list of repositories

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import {ApolloProvider} from 'react-apollo';
+import ApolloClient from 'apollo-boost';
+
 import Routes from './routes';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: async operation => {
+    operation.setContext({
+      headers: {
+        authorization: `token ad679edbe9e9aa531a9a417abb57d7aac6bde767 `,
+      },
+    });
+  },
+});
+
 export default function Main() {
-  return <Routes />;
+  return (
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
+  );
 }
--- src/screens/Trendings/index.tsx
+++ src/screens/Trendings/index.tsx
@@ -1,46 +1,42 @@
 import React from 'react';
-import {StyleSheet, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+  Text,
+  View,
+} from 'react-native';
 import Trending from 'components/Trending';
 import {
   NavigationScreenProp,
   NavigationState,
   NavigationParams,
 } from 'react-navigation';
+import gpl from 'graphql-tag';
+import {Query} from 'react-apollo';
 
-const mock = [
-  {
-    name: 'xingshaocheng / architect-awesome',
-    stars: 20,
-  },
-  {
-    name: 'dwarvesf / hidden',
-    stars: 30,
-  },
-  {
-    name: 'rasbt / python-machine-learning-book-2nd-edition',
-    stars: 36,
-  },
-  {
-    name: 'ZHacker13 / ReverseTCPShell',
-    stars: 152,
-  },
-  {
-    name: 'A3M4 / YouTube-Report',
-    stars: 152,
-  },
-  {
-    name: 'terraform-providers / terraform-provider-azurerm',
-    stars: 152,
-  },
-  {
-    name: 'Entromorgan / Autoticket',
-    stars: 152,
-  },
-  {
-    name: 'slackhq / nebula',
-    stars: 152,
-  },
-];
+const REPOSITORIES = gpl`
+{
+  search(query: "is:public", type: REPOSITORY, first: 20) {
+    nodes {
+      ... on Repository {
+        nameWithOwner
+        createdAt
+        databaseId
+        forkCount
+        url
+        description
+        issues {
+          totalCount
+        }
+        stargazers {
+          totalCount
+        }
+      }
+    }
+  }
+}
+`;
 
 export interface Props {
   navigation: NavigationScreenProp<NavigationState & NavigationParams>;
@@ -48,19 +44,32 @@ export interface Props {
 
 export default function screens(props: Props) {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      {mock.map((item: any, index: number) => {
-        return (
-          <Trending
-            key={index}
-            name={item.name}
-            stars={item.stars}
-            onPress={() => {
-              props.navigation.navigate('Details', {repository: item});
-            }}></Trending>
+    <Query query={REPOSITORIES} variables={{}}>
+      {({data, loading}: any) => {
+        return loading ? (
+          <View style={styles.loading}>
+            <ActivityIndicator
+              size={'large'}
+              color={'#000'}></ActivityIndicator>
+          </View>
+        ) : (
+          <ScrollView contentContainerStyle={styles.container}>
+            {data.search.nodes.map((item: any) => {
+              return (
+                <Trending
+                  key={item.databaseId}
+                  name={item.nameWithOwner}
+                  stars={item.stargazers.totalCount}
+                  onPress={() => {
+                    props.navigation.navigate('Details', {repository: item});
+                  }}
+                />
+              );
+            })}
+          </ScrollView>
         );
-      })}
-    </ScrollView>
+      }}
+    </Query>
   );
 }
 
@@ -69,5 +78,10 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginTop: 20,
     paddingBottom: 25,
+  },
+  loading: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
